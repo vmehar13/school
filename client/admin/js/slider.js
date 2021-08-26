56f1986596b9eed0844ba389d98975af95aaa31a
Veer Mehra <vmehar13@gmail.com>
2021-08-26 15:51:55 +0530

    26

--- client/admin/js/slider.js
+++ client/admin/js/slider.js
@@ -1,5 +1,6 @@
-let userId = getWithExpiry('userId'); 
+let userId = getWithExpiry('userId');
 let token = getWithExpiry('token');
+console.log(userId)
 
 const form = document.querySelector('.form');
 
@@ -12,19 +13,10 @@ function getWithExpiry(key) {
     const itemStr = localStorage.getItem(key)
 
     // if the item doesn't exist, return null
-    if (!itemStr) {
-        return null
-    }
+    // if (!itemStr) {
+    //     return null
+    // }
 
     const item = JSON.parse(itemStr)
-    const now = new Date()
-
-    // compare the expiry time of the item with the current time
-    if (now.getTime() > item.expiry) {
-        // If the item is expired, delete the item from storage
-        // and return null
-        localStorage.removeItem(key)
-        return null
-    }
     return item.value
 }
