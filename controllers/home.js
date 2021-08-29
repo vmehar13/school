94a746aa96365a7533209db51c2dcea10aad191f
Veer Mehra <vmehar13@gmail.com>
2021-08-30 00:45:51 +0530

    29

--- controllers/home.js
+++ controllers/home.js
@@ -64,7 +64,8 @@ exports.updateBanner = (req, res) => {
                 error: "Unable to UPDATE DATA in DB"
             })
         }
-        res.json(updateBanner)
+        // res.json(updateBanner)
+        res.redirect('/admin/slider.html')
     })
     
         
